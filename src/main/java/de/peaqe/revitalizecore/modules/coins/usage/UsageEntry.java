05dfd70fa3870d5991a6079846c6bfdf06483a8a
peaqe <contact@peaqe.dev>
2025-11-29 13:28:14 +0100

    Added UsageEntry.java for usages in /coins command

---
+++ src/main/java/de/peaqe/revitalizecore/modules/coins/usage/UsageEntry.java
@@ -0,0 +1,14 @@
+package de.peaqe.revitalizecore.modules.coins.usage;
+
+/**
+ * *
+ *
+ * @author peaqe
+ * @version 1.0
+ * @since 29.11.2025 | 12:48 Uhr
+ * *
+ */
+
+public record UsageEntry(String permission, String usage) {
+
+}
